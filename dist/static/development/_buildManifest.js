5d5e1dfee8672ec40678d1a55f09855873a7ef63
sjhong98 <sjhong98@icloud.com>
2024-01-23 15:33:03 +0900

    debug: vercel deploy option 수정

--- dist/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/board/[page]":["static\u002Fchunks\u002Fpages\u002Fboard\u002F[page].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fboard\u002F[page]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
